d7c94952ee90af9294727c1654319815a1ea70e7
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-25 20:49:57 -0400

    Update SQL-queries.sql

--- SQL-queries.sql
+++ SQL-queries.sql
@@ -6,10 +6,39 @@ Where name = "Ashley" AND year = 1999
  --result: 18164
 
 /* Are there any male Ashley's */
+
+SELECT * FROM `bigquery-public-data.usa_names.usa_1910_current`
+Where name = "Ashley" AND gender = 'M'
+
+SELECT sum(number) FROM `bigquery-public-data.usa_names.usa_1910_current`
+Where name = "Ashley" AND gender = 'M'
+
 /* When was Ashley the most popular? */
-/* What states have the most people named Ashley? */
+
+SELECT year, sum(number) AS NamedAshley
+FROM `bigquery-public-data.usa_names.usa_1910_current`
+Where name = "Ashley" 
+GROUP BY year
+ORDER BY NamedAshley DESC
+
+ -- result: 1987 with 55229 babies named Ashley
+
+/* What state has the most people named Ashley since 1910? */
+
+SELECT state, sum(number) AS NamedAshley
+FROM `bigquery-public-data.usa_names.usa_1910_current`
+Where name = "Ashley" 
+GROUP BY state
+ORDER BY NamedAshley DESC
+
+ -- result: Most babies named Ashley in California (97972), Least amount of babies named Ashley in Wyoming (1387)
+
 /* When did the name Ashley first appear? */
 
-
+SELECT year, sum(number) AS NamedAshley 
+FROM `bigquery-public-data.usa_names.usa_1910_current`
+Where name = "Ashley" 
+GROUP BY year
+ORDER BY year 
 
 Tableu: Compare male and femalre AShley popularity
