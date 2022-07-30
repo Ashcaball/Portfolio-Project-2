4523d6de7a1fcdad5848c9c66b42d6597c417c81
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-29 23:07:09 -0400

    Update Name-Queries.sql

--- Name-Queries.sql
+++ Name-Queries.sql
@@ -4,3 +4,10 @@ SELECT name, sum(number) AS number
 FROM `bigquery-public-data.usa_names.usa_1910_current` 
 GROUP BY name 
 ORDER BY number desc
+
+/* What are the most popular names in 2020?*/
+SELECT name, sum(number) AS number
+FROM `bigquery-public-data.usa_names.usa_1910_current` 
+WHERE year = 2020
+GROUP BY name 
+ORDER BY number desc;
